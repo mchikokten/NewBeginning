37b681058a6afa4cbd9128f47d5cb23bb921df74
FIRST_NAME LAST_NAME <sukiasyan_vg@hotmail.com>
2023-01-16 20:48:14 +0200

    Test commit

---
+++ src/com/ua/robot/lesson2/Main.java
@@ -0,0 +1,19 @@
+package com.ua.robot.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        int year = 2023;
+        short s;
+
+
+        s = 10;
+        byte b = 12;
+        int afterTenYears = 10;
+
+        int future = year + afterTenYears;
+//        System.out.println(s * b);
+//        System.out.println(6 % 3);
+//        System.out.println((double) s / (double) b);
+        System.out.println(++s + s);
+    }
+}
