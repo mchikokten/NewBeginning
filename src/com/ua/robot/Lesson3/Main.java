1c56f3d3c4125ab60fc550003979087d080958a5
FIRST_NAME LAST_NAME <sukiasyan_vg@hotmail.com>
2023-01-17 17:23:33 +0200

    Homework commit

--- src/com/ua/robot/Lesson3/Main.java
+++ src/com/ua/robot/Lesson3/Main.java
@@ -2,8 +2,8 @@ package com.ua.robot.Lesson3;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
+        int a = 1;
+        int b = 1;
         System.out.println(a + b);
         System.out.println(a - b);
         System.out.println(a / b);
