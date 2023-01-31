b5b80c0055f70d0aec6eee8a9f5b9b2068850dee
FIRST_NAME LAST_NAME <sukiasyan_vg@hotmail.com>
2023-01-31 23:58:32 +0200

    Lesson7 Homework commit

---
+++ src/com/ua/robot/Lesson7/Main.java
@@ -0,0 +1,27 @@
+package com.ua.robot.Lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = GetNumberTwoCubed(8);
+        System.out.println(a);
+        PrintStars(5);
+        PrintSymbolNumberOfTimes(7, "#");
+    }
+
+    static int GetNumberTwoCubed(int i) {
+        return i * i * i;
+    }
+
+    static void PrintStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    static void PrintSymbolNumberOfTimes(int number, String Symbol) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(Symbol);
+        }
+    }
+}
