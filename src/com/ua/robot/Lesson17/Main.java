251080702f6f3c3a293ece18042b3f62e4f9a54e
FIRST_NAME LAST_NAME <sukiasyan_vg@hotmail.com>
2023-03-08 22:52:41 +0200

    Lesson17 Homework commit

---
+++ src/com/ua/robot/Lesson17/Main.java
@@ -0,0 +1,10 @@
+package com.ua.robot.Lesson17;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DayOfWeek_Number dayOfWeek_Number = DayOfWeek_Number.MONDAY;
+        dayOfWeek_Number.setNumber(3);
+        dayOfWeek_Number.getDayName();
+    }
+}
